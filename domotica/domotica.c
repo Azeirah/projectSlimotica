fd4472d6b3a5582c039ef7fc262fe30fe7f20af1
MB <tijntje_7@msn.com>
2014-12-04 15:45:32 +0100

    project gemaakt

---
+++ domotica/domotica.c
@@ -0,0 +1,17 @@
+/*
+ * domotica.c
+ *
+ * Created: 04/12/2014 15:38:18
+ *  Author: MB
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
