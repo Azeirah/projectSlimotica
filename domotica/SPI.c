b23520c56a1955a5753e6cfec47da770e918c161
Jaspert <jvanmeer@xs4all.nl>
2014-12-17 13:07:21 +0100

    Added SPI library

---
+++ domotica/SPI.c
@@ -0,0 +1,136 @@
+/*
+ *             ___      ___    ___             ___   
+ *     o O O  / __|    | _ \  |_ _|           / __|  
+ *    o       \__ \    |  _/   | |      _    | (__   
+ *   TS__[O]  |___/   _|_|_   |___|   _(_)_   \___|  
+ *  {======|_|"""""|_| """ |_|"""""|_|"""""|_|"""""| 
+ * ./o--000'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-' 
+ *
+ * Created: 15-12-2014 14:08:46
+ * Author: Jasper van Meer
+ * 
+ * Module for communication via SPI
+ * Data to transmit can be set by using setDatBuffer(DATA);
+ * Data Recieved at a index of the buffer can be read using getDataBufferAtIndex(a_int); it returns -1 if number is out of bounds.
+ */ 
+	
+#include <interrupt.h>
+#include <string.h>
+
+char sendMode = 0; // 0 = Receiving || 1 = Sending
+uint8_t spiDATA = 0; //Global variable for the data recieved or to transmit
+
+//DataBuffer variables
+char dataBuffer[BUFFER_SIZE];
+char dataBufferCounter = 0;
+char dataBufferLimit = 0;
+
+/* @function : initSPI()
+ *		To Initialize SPI Communication following the parameters.
+ * @param - Refer to Explanation of SPCR in SPI.h
+ * @return - Returns the contents of the SPCR register
+ */	
+char initSPI(char clockRate, EDGE_REACT, SPI_MODE, SIGNIFICANT_BIT_FIRST, SPI_ENABLE, SPI_INTERRUPT_ENABLE) {
+	//Set Interrupt for SPI STC(Serial Transfer Complete) if SPI is to be enabled
+	if ((spiEnable & 0x1) == 1) {
+		SPSR = (1 << SPIF); //SPI Status Register : See SPI.h for Explanation
+		sei(); //Set Global Interrupt Enable : Or use SREG = (1<<7); 
+	}
+	//Set Direction registers for I/O pins
+	if ((spiMasterEnable & 0x1) == 0) {			//SPI is Slave
+		DDRB &= ~(1<<SPI_MOSI_PIN); // input
+		DDRB |= (1<<SPI_MISO_PIN); // output
+		DDRB &= ~(1<<SPI_SS_PIN); // input
+		DDRB &= ~(1<<SPI_SCK_PIN);// input
+	}
+	else if ((spiMasterEnable & 0x1) == 1) {	//SPI is Master
+		DDRB |= (1<<SPI_MOSI_PIN); // output
+		DDRB &= ~(1<<SPI_MISO_PIN); // input
+		DDRB |= (1<<SPI_SCK_PIN);// output
+		DDRB |= (1<<SPI_SS_PIN);// output
+	}
+	//Set SPI Settings : See SPI.h for explanation
+	char spiSettings = 0; //For the SPIsettings
+	spiSettings += (clockRate & 0x3);
+	spiSettings += ((edgeReact & 0x1) << SPH);
+	spiSettings += (1 << CPOL);
+	spiSettings += ((spiMasterEnable & 0x1) << MSTR);
+	spiSettings += ((dataOrder & 0x1) << DORD);
+	spiSettings += ((spiEnable & 0x1) << SPE);
+	spiSettings += ((spiInterruptEnable & 0x1) << SPIE);
+	SPCR = spiSettings; //Set SPI settings
+	return spiSettings;
+}
+
+/* @function : disableSPI()
+ *		To Disable SPI Communication
+ */
+void disableSPI(void) {
+	SPCR = 0;
+}
+
+/* @function : recievedDataSPI()
+ * @return - Returns Register SPDR's contents
+*/ 
+uint8_t getRecievedDataSPI(void) {
+	return SPDR;
+}
+
+/* @function : setSendDataSPI()
+ * @param - Data to transmit in next session
+*/ 
+void setDataToSendSPI(uint8_t dataOut) {
+	if (dataOut == '\0') { //May or may not need to be changed
+		sendMode = 0;
+		dataBuffer = 0;
+		dataBufferCounter = 0;
+		dataBufferLimit = BUFFER_SIZE;
+	}
+	SPDR = dataOut;
+}
+
+/* @function : nextData()
+ *		Set spiDATA with the next item from the buffer
+*/
+void nextData(void) {
+	if (sendMode) {
+		if (dataBufferCounter < dataBufferLimit) dataBufferCounter++;
+		spiDATA = (uint8_t) dataBuffer[dataBufferCounter];
+	}
+	if (!sendMode) {
+		if (dataBuffer < dataBufferLimit) dataBufferCounter++;
+		dataBuffer[dataBufferCounter] = (char) spiDATA;
+	}
+}
+
+/* @function : setDataBuffer()
+ *		Set the databuffer
+ * @param - A character array/string to fill the databuffer with
+*/
+void setDataBuffer(char data[]) {
+	sendMode = 1;
+	dataBuffer = data;
+	dataBufferLimit = strlen(data);
+	dataBufferCounter = 0;
+}
+
+char getDataBufferAtIndex(char a) {
+	if(a <= databufferLimit) return dataBuffer[a];
+	else return -1;
+}
+
+/* @function : ISR()
+ *		A Interrupt Service Routine; Interrupts when a successfull datatransfer occurred
+ * @param - A "vector".
+ *		SPI_STC_vect - Vector No.: 24 - Program Address: $0030
+*/ 
+ISR(SPI_STC_vect) {
+	if (sendMode) { //Set the data Register and do not read
+		setDataToSendSPI(spiDATA); 
+		nextData(); //Next 8 bits to send set in spiDATA
+	}
+	if (!sendMode) { //Read the data register and do not set
+		spiDATA = getRecievedDataSPI();
+		nextData(); //Contents of SPDR set in spiDATA
+	}
+}
---
+++ domotica/SPI.h
@@ -0,0 +1,73 @@
+/*
+ * SPI.h
+ *
+ * Created: 15-12-2014 14:09:01
+ *  Author: Jasper van Meer
+ */ 
+#ifndef SPI_H_
+#define SPI_H_
+/* Explanation:
+ * SPCR = SPI Control Register
+ * 8765 4321 bit
+ * 7654 3210 bit nr.
+ * 0000 00XX = SPR  : SPI Clock Rate Select.	2 bits, 4 possibilities
+ * 0000 0x00 = CPHA : SPI Clock Phase.			1 = React on rising edge if CPOL = 1	||	0 = React on falling edge if CPOL = 1
+ * 0000 x000 = CPOL : SPI Clock Polarity.		1 = SPI Clock high is idle				||	0 = SPI Clock low is idle
+ * 000x 0000 = MSTR : SPI Master/Slave Mode.	1 = SPI Master Mode						||	0 = SPI Slave Mode
+ * 00x0 0000 = DORD : SPI Data Order.			1 = LSB of data word Transmitted first	||	0 = MSB of data word transmitted first
+ * 0x00 0000 = SPE  : SPI Enable.				1 = SPI Enabled							||	0 = SPI Disabled
+ * x000 0000 = SPIE : SPI interrupt Enable		Refer to Datasheet : 
+ */
+
+/* Explanation:
+ * SPSR = SPI Status register
+ * 8765 4321 bit
+ * 7654 3210 bit nr.
+ * 0000 000x = SPI2X: SPI double speed bit		1 = SPI Clock now runs on double speed	||	0 = SPI Clock runs on "normal" speed
+ * 00xx xxx0 = RES  : Reserved
+ * 0x00 0000 = WCOL : Write Colision Flag		1 = If SPDR is written while transfer	||	0 = If SPDR is not written while transfer
+ * x000 0000 = SPIF : SPI Interrupt Flag		Refer to Datasheet
+ */
+
+/* Explanation:
+ * SPDR = SPI Data register
+ */
+
+//Functions
+char initSPI(char clockRate, EDGE_REACT, SPI_MODE, SIGNIFICANT_BIT_FIRST, SPI_ENABLE, SPI_INTERRUPT_ENABLE);
+void disableSPI(void); //Disable SPI
+
+uint8_t getRecievedDataSPI(void); //Return contents of SPDR
+void setDataToSendSPI(uint8_t dataOut); //Set contents of SPDR with dataOut
+
+void nextData(void); //Set next data databuffer > spiDATA || databuffer < spiDATA
+
+void setDataBuffer(char data[]); //Set the databuffer to send via SPI
+char getDataBufferAtIndex(char a); //Get Character from databuffer at index a
+
+#define BUFFER_SIZE 128
+
+//SPI Pin configurations: Conform deRFmega128 Pin Configuration
+#define SPI_MOSI_PIN	PORTB2
+#define SPI_MISO_PIN	PORTB3
+#define SPI_SS_PIN		PORTD0
+#define SPI_SCK_PIN		PORTB1
+
+//Defines for ease of use parameters of Function: initSPI();
+#define MASTER_MODE		1
+#define SLAVE_MODE		0
+#define ENABLE			1
+#define DISABLE			0
+#define RISING_EDGE		1
+#define FALLING_EDGE	0
+#define LSB_FIRST		1
+#define MSB_FIRST		0
+
+//Defines for ease of use of parameters of Function: initSPI();
+#define EDGE_REACT				char edgeReact
+#define SPI_MODE				char spiMasterEnable
+#define SIGNIFICANT_BIT_FIRST	char dataOrder
+#define SPI_ENABLE				char spiEnable
+#define SPI_INTERRUPT_ENABLE	char spiInterruptEnable
+
+#endif /* SPI_H_ */
