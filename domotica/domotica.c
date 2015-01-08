4ee5df062519820be7f8f3a2c9ca681b50a78536
Dajo Hofman <dhofman@gmx.com>
2015-01-08 20:08:52 +0100

    Starting point for further develoopment

--- domotica/domotica.c
+++ domotica/domotica.c
@@ -5,15 +5,13 @@
  *  Author: Jasper van Meer
  */ 
 
-#define F_CPU 1000000UL	//For delay Function
+//#define F_CPU 1000000UL	//For delay Function
 #include "contiki.h"	//Contiki
-//#include "I2C.h"
 #include "source/SPI.h"
 
 #include <avr/io.h>	//For pin defines
 #include <util/delay.h>	//For delay functions
 #include <stdio.h>
-//void blinkingLeds(void);
 
 //Making a Contiki Process
 PROCESS(hello_world_process, "Hello world process");
@@ -23,32 +21,14 @@ AUTOSTART_PROCESSES(&hello_world_process);
 PROCESS_THREAD(hello_world_process, ev, data) {
 	DDRE |= (1 << DDE6);
 	PROCESS_BEGIN();
-	// int x;
-	// clock_init();
+	clock_init();
 	disableSPI();
 	while (1) {
 		PORTE |= (1 << DDE6);
 		clock_delay_usec(500000);
 		PORTE &= ~(1 << DDE6);
 		clock_delay_usec(500000);
-		printf("Got event number %d\n", ev);
 		PROCESS_PAUSE();
 	}
 	PROCESS_END();
 }
-
-/*
-void blinkingLeds(void) {
-	
-	int x;
-	
-	printf("Hello contiki world");
-	for(x = 0; x < 10000 ; x++) {
-		PROCESS_PAUSE();
-		PORTE |= (1 << DDE6);
-		clock_delay_usec(500000);
-		PORTE &= ~(1 << DDE6);
-		clock_delay_usec(500000);
-	}
-}
-*/
